d38015c689f2437ad9aebf572fef5ce3aa4abfae
brkusaki <ukbbze@gmail.com>
2026-09-19 03:38:24 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -66,7 +66,7 @@ self.addEventListener('notificationclick', function (event) {
   );
 });
 
-const CACHE = 'angatubaon-v347';
+const CACHE = 'angatubaon-v348';
 const STATIC = [
   '/',
   '/index.html',
